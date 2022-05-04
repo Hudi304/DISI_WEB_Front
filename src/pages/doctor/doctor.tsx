17af290dcdd3884a6fadd55a5f9e9da7a024a194
Andrei Sasaran <sasaranandreipaul@gmail.com>
2022-05-04 19:20:39 +0300

    Fix routing

--- src/pages/doctor/doctor.tsx
+++ src/pages/doctor/doctor.tsx
@@ -1,57 +1,19 @@
-import { useState } from "react";
+import { ICONS } from "components/icon/icon";
+import { NavBar, NavBarBtn } from "components/nav-bar/nav-bar";
+import { Route, Routes } from "react-router-dom";
+import { ConfirmationDonation } from "./confirm-donation/confirm-donation";
 import "./doctor.scss";
 
+const navButtons: NavBarBtn[] = [
+  { icon: ICONS.USER, name: "ConfirmationDonation", path: "/main/doctor/confirm_donation" },
+];
+
 export const Doctor = () => {
-  const [title, setTitle] = useState("");
-  const [text, setText] = useState("");
-  
-  return (
-    <div className="flex items-center justify-center ">
-      <div className="my-profile-page-container-without-grid-template ">
-        <div className="my-profile-side-bar debug">
-          <div className="grid grid-rows-12">
-            <form className="px-10">
-              <br />
-              <label className="px-2 text-headline font-bold">Donor Name *</label>
-              <br />
-              <input
-                name="donor-name"
-                value={title}
-                onChange={(e) => {
-                  setTitle(e.target.value);
-                }}
-              />
-              <br />
-              <br />
-              <label className="px-2 text-headline font-bold">Center Name *</label>
-              <br />
-              <input
-                name="center-name"
-                value={text}
-                onChange={(e) => {
-                  setText(e.target.value);
-                }}
-              />
-              <br />
-              <br />
-              <label className="px-2 text-headline font-bold">Request ID</label>
-              <br />
-              <input
-                name="center-name"
-                value={text}
-                onChange={(e) => {
-                  setText(e.target.value);
-                }}
-              />
-              <br />
-              <br />
-              <div className="flex items-center justify-center">
-                <input className="mt-5 px-3" type="submit" value="Confirm Donation" />
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="h-full">
+    <NavBar buttons={navButtons} />
+    DOCTOR PAGE
+      <Routes>
+        <Route path="/confirm_donation" element={<ConfirmationDonation />} />
+      </Routes>
+  </div>;
 };
