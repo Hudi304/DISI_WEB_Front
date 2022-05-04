8d2e99de2dc30dfcc53915518391c57b4aba1e4c
Vlad Ilie Filip <vladiliefilip@icloud.com>
2022-05-05 01:45:31 +0300

    small chnage

--- src/api/endpoints/my-profile-controller.api.ts
+++ src/api/endpoints/my-profile-controller.api.ts
@@ -1,6 +1,6 @@
 import { UserProfileDTO } from 'common/models/UserProfileDTO';
 import { API } from "../api";
 
-// export const getUserProfileApi = (id: string): Promise<any> => API().get(`my-profile/${userId}?id=${id}`);
+export const getUserProfileApi = (id: string): Promise<any> => API().get(`my-profile/${userId}?id=${id}`);
 
-// export const updateUserProfileApi = (id: string, newUserProfile: UserProfileDTO): Promise<any> => API().put(`my-profile/${userId}?id=${id}&newUserProfile=${newUserProfile}`);
+export const updateUserProfileApi = (id: string, newUserProfile: UserProfileDTO): Promise<any> => API().put(`my-profile/${userId}?id=${id}&newUserProfile=${newUserProfile}`);
